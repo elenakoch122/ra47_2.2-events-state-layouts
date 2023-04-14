3fada6075cf863173e40e2c4abd0b629c81ba1b1
Elena Kochegarova <lelet@ngs.ru>
2023-04-14 22:02:07 +0700

    added a key to the component

--- src/components/CardsView.js
+++ src/components/CardsView.js
@@ -1,9 +1,10 @@
 import ShopCard from "./ShopCard";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function CardsView({ cards }) {
   return (
     <div className="cards">
-      {cards.map(c => <ShopCard card={c}/>)}
+      {cards.map(c => <ShopCard card={c} key={uuidv4()} />)}
     </div>
   );
 }
--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,9 +1,10 @@
 import ShopItem from "./ShopItem";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function ListView({ items }) {
   return (
     <div className="items">
-      {items.map(i => <ShopItem item={i} />)}
+      {items.map(i => <ShopItem item={i} key={uuidv4()} />)}
     </div>
   );
 }
